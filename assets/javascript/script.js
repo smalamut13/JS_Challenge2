f037feaf2e625acda5a9eef2d6e6ca1e5af3cfc1
Samantha Malamut <smalamut13@gmail.com>
2015-11-02 21:15:36 -0500

    First commit

---
+++ assets/javascript/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+	var index
+	var name = ["Ralph", "Zack", "Tony"];
+
+	var grade = ["90%", "80%", "100%"];
+	
+	for (index = 0; index < name.length; index++) {
+		console.log(name[index] + " got " + grade[index] + " on the test.");
+	}
+});
+	
+
+	
